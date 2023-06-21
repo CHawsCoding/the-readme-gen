91112d2faf93f0a494f82c184e2ecbd113672a6f
Collin Haws <117249176+CHawsCoding@users.noreply.github.com>
2023-06-20 19:31:36 -0600

    completed functions in index.js - and tested by runnning and logging heelow world to the console

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,26 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const generateMarkdown = require('./generateMarkdown')
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the title of your project:',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description for your project:',
+      },
+      {
+        type: 'input',
+        name: '',
+        message: '',
+      },
+      // Add more questions here as needed
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +30,6 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+console.log("Hello World")
